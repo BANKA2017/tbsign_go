b59cecaba6063d9c4ce69fadd8feb4a18876f991
BANKA2017 <kdwnil@hotmail.com>
2023-11-07 16:49:10 +0800

    ~ fix resign func + limit for each table

--- plugins/sign.go
+++ plugins/sign.go
@@ -25,29 +25,30 @@ var tableList = []string{"tieba"}
 var today string
 
 func Dosign(table string, retry bool) (bool, error) {
-	//signMode := GetOption(options, "sign_mode")
+	//signMode := GetOption(options, "sign_mode")// client mode only
+	hasFailed := false
 	signHour, _ := strconv.ParseInt(_function.GetOption("sign_hour"), 10, 64)
 	if int64(_function.Now.Hour()) <= signHour {
 		log.Println("sign:", strconv.FormatInt(signHour, 10)+"点时忽略签到")
-		return true, nil
+		return hasFailed, nil
 	}
-	//limit := GetOption(options, "cron_limit")
+	limit, _ := strconv.ParseInt(_function.GetOption("cron_limit"), 10, 64)
 	var tiebaList []model.TcTieba
-	//处理所有未签到的贴吧
-	//TODO support limit
-	//if limit == "0" {
+
+	if limit == 0 {
+		limit = -1
+	}
 	if retry {
-		_function.GormDB.Where("no = ? AND latest != ? AND status IN ?", 0, _function.Now.Local().Day(), []int64{340011, 2280007, 110001, 1989004, 255}).Find(&tiebaList)
+		// 重签
+		_function.GormDB.Where("no = ? AND latest != ? AND status IN ?", 0, _function.Now.Local().Day(), []int64{340011, 2280007, 110001, 1989004, 255}).Limit(int(limit)).Find(&tiebaList)
 	} else {
-		_function.GormDB.Where("no = ? AND latest != ?", 0, _function.Now.Local().Day()).Find(&tiebaList)
+		_function.GormDB.Where("no = ? AND latest != ?", 0, _function.Now.Local().Day()).Limit(int(limit)).Find(&tiebaList)
 	}
 
 	if len(tiebaList) <= 0 {
 		log.Println("sign: Empty list")
-		return true, nil
+		return hasFailed, nil
 	}
-	//} else {
-	//}
 
 	//log.Println(tiebaList)
 	sleep, _ := strconv.ParseInt(_function.GetOption("sign_sleep"), 10, 64)
@@ -89,6 +90,9 @@ func Dosign(table string, retry bool) (bool, error) {
 					Status:    int32(errorCode),
 					LastError: errorMsg,
 				})
+			} else if err != nil {
+				log.Println(err)
+				hasFailed = true
 			}
 
 			log.Println("sign:", pid, kw, fid, id, now.Local().Day(), time.Now().UnixMilli()-now.UnixMilli())
@@ -99,12 +103,11 @@ func Dosign(table string, retry bool) (bool, error) {
 	}
 	wg.Wait()
 	log.Println("sign: done!", time.Now().UnixMilli()-_function.Now.UnixMilli())
-	return false, nil
+	return hasFailed, nil
 }
 
 func DoSignAction() {
 	today = _function.Now.Local().Format("2006-01-02")
-	limit, _ := strconv.ParseInt(_function.GetOption("cron_limit"), 10, 64)
 	cornSignAgain := _function.GetOption("cron_sign_again")
 	cornSignAgainParsed, err := gophp.Unserialize([]byte(cornSignAgain))
 	if err != nil {
@@ -136,16 +139,12 @@ func DoSignAction() {
 		//log.Println(string(cornSignAgainEncoded))
 	}
 
-	for index, table := range tableList {
-		if limit > 0 && index > int(limit) {
-			break
-		}
+	for _, table := range tableList {
 		Dosign(table, false)
 	}
 
 }
 
-// TODO resign
 func DoReSignAction() {
 	retryMax, _ := strconv.ParseInt(_function.GetOption("retry_max"), 10, 64)
 
@@ -159,8 +158,8 @@ func DoReSignAction() {
 		for retryMax == 0 || int64(retryNum) <= retryMax {
 			retryAgain := false
 			for _, table := range tableList {
-				checkRequest, _ := Dosign(table, true)
-				if checkRequest {
+				hasFailed, _ := Dosign(table, true)
+				if hasFailed {
 					retryAgain = true
 				}
 			}
