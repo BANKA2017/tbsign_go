88ca316c8651d82f0557f1fc73096e3fe69f541c
BANKA2017 <kdwnil@hotmail.com>
2024-07-01 16:00:42 +0800

    fix: throw error when a SQLite file is not exists

--- functions/db.go
+++ functions/db.go
@@ -21,37 +21,39 @@ func ConnectToSQLite(path string, logLevel logger.LogLevel, servicePrefix string
 	var readDBHandle = new(gorm.DB)
 	var err error
 	if _, err = os.Stat(path); err == nil {
-		// sqlite
-		// write
-
-		writeDBHandle, err = gorm.Open(sqlite.Open(path), &gorm.Config{
-			Logger: logger.Default.LogMode(logLevel),
-		})
-		if err != nil {
-			log.Println(servicePrefix+" w:", err)
-		}
-		connw, err := writeDBHandle.DB()
-		connw.SetMaxOpenConns(1)
-
-		if err != nil {
-			log.Println(servicePrefix+" w:", err)
-		}
-
-		//read
-		readDBHandle, err = gorm.Open(sqlite.Open(path), &gorm.Config{
-			Logger: logger.Default.LogMode(logLevel),
-		})
-		if err != nil {
-			log.Println(servicePrefix+" r:", err)
-		}
-		// connr, err := readDBHandle.DB()
-		// connr.SetMaxOpenConns(max(4, runtime.NumCPU()))
-
-		if err != nil {
-			log.Println(servicePrefix+" r:", err)
-		}
-		log.Println(servicePrefix + ": sqlite connected!")
+		log.Println("db:", path, "is not exists")
 	}
+	// sqlite
+	// write
+
+	writeDBHandle, err = gorm.Open(sqlite.Open(path), &gorm.Config{
+		Logger: logger.Default.LogMode(logLevel),
+	})
+	if err != nil {
+		log.Println(servicePrefix+" w:", err)
+	}
+	connw, err := writeDBHandle.DB()
+	connw.SetMaxOpenConns(1)
+
+	if err != nil {
+		log.Println(servicePrefix+" w:", err)
+	}
+
+	//read
+	readDBHandle, err = gorm.Open(sqlite.Open(path), &gorm.Config{
+		Logger: logger.Default.LogMode(logLevel),
+	})
+	if err != nil {
+		log.Println(servicePrefix+" r:", err)
+	}
+	// connr, err := readDBHandle.DB()
+	// connr.SetMaxOpenConns(max(4, runtime.NumCPU()))
+
+	if err != nil {
+		log.Println(servicePrefix+" r:", err)
+	}
+	log.Println(servicePrefix + ": sqlite connected!")
+
 	writeDBHandle.Exec("PRAGMA journal_mode = WAL;PRAGMA busy_timeout = 5000;PRAGMA synchronous = NORMAL;PRAGMA cache_size = 100000;PRAGMA foreign_keys = true;PRAGMA temp_store = memory;")
 	return readDBHandle, writeDBHandle, err
 }
